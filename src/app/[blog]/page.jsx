4a102925c7339611a39676ee52cd7f9a4ce3334b
ibrahim-sert <evo3506@gmail.com>
2024-01-21 18:51:12 +0300

    optional-catch-all-routes

--- src/app/[blog]/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Blog = ({ params }) => {
-  return <div>Blog Sayfası {params.blog}</div>;
-};
-
-export default Blog;
---
+++ src/app/docs/[[...post]]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostPage = ({ params }) => {
+  return <div>PostPage {params.post}</div>;
+};
+
+export default PostPage;
